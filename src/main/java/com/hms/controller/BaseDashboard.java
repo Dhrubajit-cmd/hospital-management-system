18fc3f36c1ced808df53ef865fc859028d0378ec
Dhrubajit-cmd <dhrubajitchakravorty@gmail.com>
2026-04-08 11:28:23 +0530

    Made Some Changes

--- src/main/java/com/hms/controller/BaseDashboard.java
+++ src/main/java/com/hms/controller/BaseDashboard.java
@@ -2,12 +2,9 @@ package main.java.com.hms.controller;
 
 import javax.swing.JFrame;
 
-/**
- * A base class for all dashboard views to demonstrate object-oriented programming (OOP).
- * It holds common variables that subclasses can access using the 'protected' modifier.
- */
 public class BaseDashboard extends JFrame {
-    // This is the protected variable that subclasses (Doctor, Patient, Staff dashboards) can use
+    // This is the protected variable that subclasses (Doctor, Patient, Staff
+    // dashboards) can use
     protected int userId;
 
     public BaseDashboard(int userId) {
